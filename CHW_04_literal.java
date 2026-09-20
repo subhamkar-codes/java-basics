43d1b89a96dd84b06283f68b5884e36f950548bb
Subham Kar <karsubham669@gmail.com>
2026-09-20 09:50:22 +0530

    Add Java basics

---
+++ CHW_04_literal.java
@@ -0,0 +1,9 @@
+public class CHW_04_literal {
+    public static void main(String[] args) {
+    byte age = 20;
+    char gender = 'M';
+    float f1 = 5.6f;
+    String sk = "wsubham";
+    System.out.println(age + "+" + gender + "+" + f1 + "+" + sk);
+    }
+}
