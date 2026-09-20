994eea02b09fa9df8b206e02a5a29a6cee01a8bf
Subham Kar <karsubham669@gmail.com>
2026-09-20 09:49:41 +0530

    Add Java basics

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 300;
+        int b = 100;
+        int c = 100;
+        int d = a + b + c;
+        System.out.println("The Sum of "+ a + " and " +b + " and " +c + " is " + d );
+    }
+}
