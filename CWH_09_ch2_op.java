3419c8d357b6e16c15d2b71d81b52e43f1ced2db
Subham Kar <karsubham669@gmail.com>
2026-09-24 15:39:54 +0530

    Add Java basics

---
+++ CWH_09_ch2_op.java
@@ -0,0 +1,17 @@
+public class CWH_09_ch2_op {
+    static void main() {
+    int a = 6 * 5 - 34/2;
+    int b = 60/5 - 32 *2;
+        System.out.println(a);
+        System.out.println(b);
+
+        //problem1
+        int x = 6;
+        int y = 4;
+        System.out.println((x - y)/2);
+
+
+       // problem 2
+
+    }
+}
